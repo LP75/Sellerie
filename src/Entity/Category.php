8ef883d44cb857bb955ab15ebb86e9f6458c976e
Loïc Plantive <lo.pla231@gmail.com>
2024-10-15 10:19:03 +0200

    EquipmentType Creation & Category Changes

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -15,6 +17,17 @@ class Category
 
     #[ORM\Column(length: 255)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, EquipmentType>
+     */
+    #[ORM\OneToMany(targetEntity: EquipmentType::class, mappedBy: 'category_id')]
+    private Collection $equipmentTypes;
+
+    public function __construct()
+    {
+        $this->equipmentTypes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Category
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, EquipmentType>
+     */
+    public function getEquipmentTypes(): Collection
+    {
+        return $this->equipmentTypes;
+    }
+
+    public function addEquipmentType(EquipmentType $equipmentType): static
+    {
+        if (!$this->equipmentTypes->contains($equipmentType)) {
+            $this->equipmentTypes->add($equipmentType);
+            $equipmentType->setCategoryId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEquipmentType(EquipmentType $equipmentType): static
+    {
+        if ($this->equipmentTypes->removeElement($equipmentType)) {
+            // set the owning side to null (unless already changed)
+            if ($equipmentType->getCategoryId() === $this) {
+                $equipmentType->setCategoryId(null);
+            }
+        }
 
         return $this;
     }
