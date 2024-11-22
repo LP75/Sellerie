b363412a266c3c3a4241530e0962c82c033eadfb
Loïc Plantive <lo.pla231@gmail.com>
2024-11-22 09:42:57 +0100

    edit form style equipmentItemType

--- src/Form/EquipmentItemType.php
+++ src/Form/EquipmentItemType.php
@@ -1,15 +1,16 @@
 <?php
+// src/Form/EquipmentItemType.php
 
 namespace App\Form;
 
-use App\Entity\EquipmentItem;
-use App\Entity\EquipmentType;
-use App\Entity\Location;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\EquipmentItem;
+use App\Entity\EquipmentType;
+use App\Entity\Location;
 use App\Enum\EquipmentState;
 
 class EquipmentItemType extends AbstractType
@@ -26,17 +27,34 @@ class EquipmentItemType extends AbstractType
                     'Hors service' => EquipmentState::HORS_SERVICE,
                     'En location' => EquipmentState::EN_LOCATION,
                 ],
-                'choice_label' => function ($choice, $key, $value) {
-                    return $choice->getState();
-                },
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+                ],
+                'row_attr' => [
+                    'class' => 'mb-4 sm:mb-5',
+                ],
             ])
             ->add('equipmentType', EntityType::class, [
                 'class' => EquipmentType::class,
-                'choice_label' => 'id',
+                'choice_label' => function (EquipmentType $equipmentType) {
+                    return $equipmentType->getName() . ' - ' . $equipmentType->getBrand();
+                },
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+                ],
+                'row_attr' => [
+                    'class' => 'mb-4 sm:mb-5',
+                ],
             ])
             ->add('location', EntityType::class, [
                 'class' => Location::class,
                 'choice_label' => 'id',
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
+                ],
+                'row_attr' => [
+                    'class' => 'mb-4 sm:mb-5',
+                ],
             ])
         ;
     }
