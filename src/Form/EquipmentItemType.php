a5ca65ed3c3c7a087ef53a2e491aa953f1bbf88e
Loïc Plantive <lo.pla231@gmail.com>
2024-12-12 18:06:14 +0100

    Front :

--- src/Form/EquipmentItemType.php
+++ src/Form/EquipmentItemType.php
@@ -56,7 +56,9 @@ class EquipmentItemType extends AbstractType
             ])
             ->add('location', EntityType::class, [
                 'class' => Location::class,
-                'choice_label' => 'id',
+                'choice_label' => function (Location $location) {
+                    return $location->getAisle() . ' - ' . $location->getShelf();
+                },
                 'attr' => [
                     'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500',
                 ],
